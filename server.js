fb6aae51f37231587026c054a0871b93db177303
abimwaqas <muhammad.waqas@trax.pk>
2021-11-27 18:37:09 +0500

    initial add functionality done

--- server.js
+++ server.js
@@ -7,12 +7,16 @@ const app = express()
 const expressLayouts = require("express-ejs-layouts")
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json())
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/mybrary');
@@ -22,5 +26,6 @@ db.once('open', () => console.log('Connected to Mongoose'))
 
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
